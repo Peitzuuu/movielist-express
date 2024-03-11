1a74f435321be05aafede7cffb24a52e570a128b
Peitzuuu <patty80937@gmail.com>
2024-03-11 23:20:55 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -16,12 +16,13 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies', (req, res) => {
-  res.render('index', { movies: movies, BASE_IMG_URL})
+  res.render('index', { movies: movies, BASE_IMG_URL })
 })
 
 app.get('/movie/:id', (req, res) => {
   const id = req.params.id
-  res.send(`Reading movie: ${id}`)
+  const movie = movies.find((mv) => mv.id.toString() === id)
+  res.render('detail', { movie , BASE_IMG_URL })
 })
 
 app.listen(port, () => {
